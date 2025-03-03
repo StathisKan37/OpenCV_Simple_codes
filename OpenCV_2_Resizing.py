8126f2e55b31ed8a28bcd4cc8cc601d9f0128db3
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-03-03 12:47:09 +0200

    Update OpenCV_2_Resizing.py

--- OpenCV_2_Resizing.py
+++ OpenCV_2_Resizing.py
@@ -1,19 +1,19 @@
-import cv2 as cv
+import cv2
 
 # Reading an 500x500 png image and saving it in the variable 'img'
-img=cv.imread('demo_image.png')
+img=cv2.imread('demo_image.png')
 
 # Showing the image in the normal size
-cv.imshow('500x500 image', img)
+cv2.imshow('500x500 image', img)
 
 # Scaling the image and shwoing the result
 # 250x250
-img_1 = cv.resize(img, (250,250), interpolation=cv.INTER_AREA)
-cv.imshow('250x250 resized', img_1)
+img_1 = cv.resize(img, (250,250), interpolation=cv2.INTER_AREA)
+cv2.imshow('250x250 resized', img_1)
 # 750x750
-img_2 = cv.resize(img, (750,750), interpolation=cv.INTER_AREA)
-cv.imshow('750x750 resized', img_2)
+img_2 = cv.resize(img, (750,750), interpolation=cv2.INTER_AREA)
+cv2.imshow('750x750 resized', img_2)
 
 # Waiting for key 0 to destroy all windows
-cv.waitKey(0)
-cv.destroyAllWindows() 
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
