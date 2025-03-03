6646d0a2bdc7051856d957a9d00b3d109e952ff7
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-03-03 12:45:32 +0200

    Update OpenCV_1_Loading.py

--- OpenCV_1_Loading.py
+++ OpenCV_1_Loading.py
@@ -1,14 +1,14 @@
 # Importing the cv2 library as cv
-import cv2 as cv
+import cv2
 
 # Printing the version of OpenCV
-print("OpenCV version:", cv.__version__)
+print("OpenCV version:", cv2.__version__)
 
 # Reading the demo image and saving it in the variable 'img'
-img = cv.imread('demo_image.png',cv.IMREAD_COLOR)
+img = cv2.imread('demo_image.png',cv2.IMREAD_COLOR)
 
 # Reading the demo image in grayscale and saving it
-img_gray = cv.imread('demo_image.png',cv.IMREAD_GRAYSCALE)
+img_gray = cv2.imread('demo_image.png',cv2.IMREAD_GRAYSCALE)
 
 # Alternatively:
 # IMREAD_COLOR: 1
@@ -16,11 +16,11 @@ img_gray = cv.imread('demo_image.png',cv.IMREAD_GRAYSCALE)
 # IMREAD_UNCHANGED: -1
 
 # Displaying the images inside a window
-cv.imshow('Demo image',img)  
-cv.imshow('Grayscale image',img_gray)
+cv2.imshow('Demo image',img)  
+cv2.imshow('Grayscale image',img_gray)
  
 # Waiting for a keystroke
-cv.waitKey(0)  
+cv2.waitKey(0)  
  
 # Destroying all the windows created
-cv.destroyAllWindows() 
+cv2.destroyAllWindows() 
